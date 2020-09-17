d62cdfd1553249233b2f6eafe60b045164aa78b7
Sylv Zar <sazarzycki@gmail.com>
2020-09-17 14:09:14 +0100

    Local storage bug fix

--- script copy.js
+++ script copy.js
@@ -1,41 +1,42 @@
-// Book Class: Represents a Book
+// Book Class - Constructor to Represent a Book
 class Book {
-  constructor(title, author, isbn) {
+  constructor(isbn, title, author) {
+    this.isbn = isbn;
     this.title = title;
     this.author = author;
-    this.isbn = isbn;
   }
 }
 
-// UI Class: Handle UI Tasks
+// UI Class - Handling UI Tasks
 class UI {
+  // Display books
   static displayBooks() {
     const books = Store.getBooks();
 
     books.forEach((book) => UI.addBookToList(book));
   }
 
+  // Add books to list
+  // create tr element with inner html template
   static addBookToList(book) {
     const list = document.querySelector('#book-list');
 
     const row = document.createElement('tr');
-    row.classList.add('book-row');
-    row.dataset.isbn = book.isbn;
 
     row.innerHTML = `
-      <td>${book.title}</td>
-      <td>${book.author}</td>
-      <td>${book.isbn}</td>
-      <td><a href="#" class="btn btn-danger btn-sm delete" data-isbn="${book.isbn}">X</a></td>
-    `;
+<td>${book.isbn}</td>
+<td>${book.title}</td>
+<td>${book.author}</td>
+<td><a href="#" class="btn btn-warning btn-sm delete">X</a></td>
+`;
 
     list.appendChild(row);
   }
 
+  // remove a book from list
   static deleteBook(el) {
     if (el.classList.contains('delete')) {
-      const isbn = el.dataset.isbn;
-      document.querySelector(`.book-row[data-isbn="${isbn}"]`).remove();
+      el.parentElement.parentElement.remove();
     }
   }
 
@@ -43,21 +44,25 @@ class UI {
     const div = document.createElement('div');
     div.className = `alert alert-${className}`;
     div.appendChild(document.createTextNode(message));
+    // placement of the div alert element within the container
     const container = document.querySelector('.container');
     const form = document.querySelector('#book-form');
+    // insert div before the form within the container
     container.insertBefore(div, form);
-
-    // Vanish in 3 seconds
+    // 3 sec timeout for alerts
     setTimeout(() => document.querySelector('.alert').remove(), 3000);
   }
 
+  // clear form fields function
   static clearFields() {
     document.querySelector('#title').value = '';
     document.querySelector('#author').value = '';
     document.querySelector('#isbn').value = '';
+    // document.getElementById('#book-form').requestFullscreen();
   }
 }
 
+// Store Class - Storage
 // Store Class: Handles Storage
 class Store {
   static getBooks() {
@@ -90,10 +95,10 @@ class Store {
   }
 }
 
-// Event: Display Books
+// Event - display books upon loading document
 document.addEventListener('DOMContentLoaded', UI.displayBooks);
 
-// Event: Add a Book
+//Add a book
 document.querySelector('#book-form').addEventListener('submit', (e) => {
   // Prevent actual submit
   e.preventDefault();
@@ -103,20 +108,20 @@ document.querySelector('#book-form').addEventListener('submit', (e) => {
   const author = document.querySelector('#author').value;
   const isbn = document.querySelector('#isbn').value;
 
-  // Validate
+  // Validation
   if (title === '' || author === '' || isbn === '') {
     UI.showAlert('Please fill in all fields', 'danger');
   } else {
-    // Instatiate book
-    const book = new Book(title, author, isbn);
+    // Instantiate book
+    const book = new Book(isbn, title, author);
 
-    // Add Book to UI
+    // Add book to UI
     UI.addBookToList(book);
 
     // Add book to store
     Store.addBook(book);
 
-    // Show success message
+    // Show success alert
     UI.showAlert('Book Added', 'success');
 
     // Clear fields
