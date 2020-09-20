8a6d18de595525d69e0d4cdbf97d2669d11ad70e
Sylv Zar <sazarzycki@gmail.com>
2020-09-20 20:17:41 +0100

    Changed visuals

--- script.js
+++ script.js
@@ -9,56 +9,45 @@ class Book {
 
 // UI Class: Handle UI Tasks
 class UI {
-  // Display books
   static displayBooks() {
     const books = Store.getBooks();
 
     books.forEach((book) => UI.addBookToList(book));
   }
 
-  // Add books to list
   static addBookToList(book) {
     const list = document.querySelector('#book-list');
 
-    //create a tr element for new book
     const row = document.createElement('tr');
-    row.classList.add('book-row');
-    row.dataset.isbn = book.isbn;
 
     row.innerHTML = `
-      <td>${book.isbn}</td>
       <td>${book.title}</td>
       <td>${book.author}</td>
-      <td><a href="#" class="btn btn-danger btn-sm delete" data-isbn="${book.isbn}">X</a></td>
+      <td>${book.isbn}</td>
+      <td><a href="#" class="btn btn-danger btn-sm delete">X</a></td>
     `;
 
     list.appendChild(row);
   }
 
-  // remove book from list
   static deleteBook(el) {
     if (el.classList.contains('delete')) {
-      const isbn = el.dataset.isbn;
-      document.querySelector(`.book-row[data-isbn="${isbn}"]`).remove();
+      el.parentElement.parentElement.remove();
     }
   }
 
-  // Alert upon submit
   static showAlert(message, className) {
     const div = document.createElement('div');
     div.className = `alert alert-${className}`;
     div.appendChild(document.createTextNode(message));
-    // placement of the div within the container
     const container = document.querySelector('.container');
     const form = document.querySelector('#book-form');
-    // insert div before the form within the container
     container.insertBefore(div, form);
 
-    // 3 sec timeout for alerts
+    // Vanish in 3 seconds
     setTimeout(() => document.querySelector('.alert').remove(), 3000);
   }
 
-  //clear form fields
   static clearFields() {
     document.querySelector('#title').value = '';
     document.querySelector('#author').value = '';
@@ -98,10 +87,10 @@ class Store {
   }
 }
 
-// Event - display books on loading document
+// Event: Display Books
 document.addEventListener('DOMContentLoaded', UI.displayBooks);
 
-// Event - add a book
+// Event: Add a Book
 document.querySelector('#book-form').addEventListener('submit', (e) => {
   // Prevent actual submit
   e.preventDefault();
@@ -132,7 +121,7 @@ document.querySelector('#book-form').addEventListener('submit', (e) => {
   }
 });
 
-// Event - Remove a Book
+// Event: Remove a Book
 document.querySelector('#book-list').addEventListener('click', (e) => {
   // Remove book from UI
   UI.deleteBook(e.target);
